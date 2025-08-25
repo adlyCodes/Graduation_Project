78ba46654ac5f7cbe2df6c55e1f8b43cc4a44475
Ahmed Adly <113637234+adlyCodes@users.noreply.github.com>
2025-08-25 03:24:00 +0300

    Delete srsms directory

--- srsms/src/main/java/com/pica/srsms/Repository/SRSDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pica.srsms.Repository;
-
-import com.pica.srsms.Entity.SRSDocument;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SRSDocumentRepository extends JpaRepository<SRSDocument, Long> {
-}
